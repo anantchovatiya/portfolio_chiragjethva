57023b63e03ac74c67dde0007ceb090dc9ae725a
anantchovatiya <147299670+anantchovatiya@users.noreply.github.com>
2024-04-07 10:07:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require("path");
 const dotenv = require('dotenv').config()
 
 const twilio = require('twilio')
-const accountSid = process.env.SID;
-const authToken = process.env.TOKEN;
+const accountSid = ACefd751ba661d37a9e2ee8042d8e5aaf3
+const authToken = d8eb20e08131951c21cb6090be6ff164;
 const twilioClient = new twilio.Twilio(accountSid, authToken);
 
 const ejsMate = require("ejs-mate");
